ec4bb8a319acd59a9f861404f572602a07942127
shuyun <1012160677@qq.com>
2024-08-07 22:46:47 +0800

    #20240807#ajax#json形式请求和响应

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/main/java/com/atguigu/crowd/mvc/handler/AjaxTestHandler.java
@@ -0,0 +1,23 @@
+package com.atguigu.crowd.mvc.handler;
+
+import com.atguigu.crowd.entity.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AjaxTestHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(AjaxTestHandler.class);
+
+    @RequestMapping("/send/object/student.html")
+    @ResponseBody
+    public String ajaxTest(@RequestBody Student list){
+        log.info(list.toString());
+//        list.stream().forEach(System.out::println);
+        return "target";
+    }
+}

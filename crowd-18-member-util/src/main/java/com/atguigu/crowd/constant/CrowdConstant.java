b4625cdc815674901464b83407d7dc916c64f47f
shuyun <1012160677@qq.com>
2024-10-13 18:58:14 +0800

    #20241013#前台#会员登录#发送短信验证码

--- crowd-18-member-util/src/main/java/com/atguigu/crowd/constant/CrowdConstant.java
+++ crowd-18-member-util/src/main/java/com/atguigu/crowd/constant/CrowdConstant.java
@@ -5,6 +5,7 @@ package com.atguigu.crowd.constant;
  */
 public class CrowdConstant {
 
+    //验证码存入到redis中的key的前缀
     public static final String REDIS_MESSAGE_CODE_PREFIX = "REDIS_MESSAGE_CODE_PREFIX_";
 
 }

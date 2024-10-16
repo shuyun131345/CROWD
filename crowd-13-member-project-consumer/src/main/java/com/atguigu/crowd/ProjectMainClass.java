fec373395fe1ded9698aa87848351f96e5a5c442
shuyun <1012160677@qq.com>
2024-10-16 15:34:44 +0800

    #20241016#前台#会员登录#Project工程

---
+++ crowd-13-member-project-consumer/src/main/java/com/atguigu/crowd/ProjectMainClass.java
@@ -0,0 +1,12 @@
+package com.atguigu.crowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMainClass.class);
+        System.out.println("Hello world!");
+    }
+}
